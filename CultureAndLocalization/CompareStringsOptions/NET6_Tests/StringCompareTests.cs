b1f929fc5e46b5beff7d4a53f5a1abb5e3fbac1c
Wojciech Kwapisiewicz <wojciech.kwapisiewicz@gmail.com>
2025-11-15 13:37:38 +0100

    Quick tests for characters with accentes comparison for culture aware string comparisone

---
+++ CultureAndLocalization/CompareStringsOptions/NET6_Tests/StringCompareTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace NET6_Tests
+{
+    public class CompareIgnoreCaseAndAccent
+    {
+        private ITestOutputHelper _output;
+
+        public CompareIgnoreCaseAndAccent(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void StringCompare_IgnoreNonSpaceForVariantsOfE_OutputIfEqual()
+        {
+            var charactersWithAccents = new List<string>()
+            {
+                "C", "é", "è",  "ê", "ë", "e", "E", "Ę", "ę"
+            };
+
+            for (int i = 0; i < charactersWithAccents.Count; i++)
+            {
+                for (int j = i + 1; j < charactersWithAccents.Count; j++)
+                {
+                    _output.WriteLine(string.Format("Comparison result {0} for: {1}  {2}",
+                        string.Compare(charactersWithAccents[i], charactersWithAccents[j],
+                            CultureInfo.InvariantCulture, CompareOptions.IgnoreCase | CompareOptions.IgnoreNonSpace),
+                        charactersWithAccents[i],
+                        charactersWithAccents[j]));
+                }
+            }
+        }        
+    }
+}
